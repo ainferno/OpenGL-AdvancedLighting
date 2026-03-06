c5994f824eed652b8f7b59d834ea7f3d9f0e90c0
ainferno <vasily.nichiporenko@ya.ru>
2026-03-06 11:02:44 +0300

    changed simple paralax mapping to paralax occlusion mapping

--- 6.paralax_mapping/normal_mapping.fs
+++ 6.paralax_mapping/normal_mapping.fs
@@ -16,9 +16,34 @@ uniform sampler2D depthMap;
 uniform float heightScale;
 
 vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir) { 
-    float height =  texture(depthMap, texCoords).r;
+    const float minLayers = 8.0;
+    const float maxLayers = 32.0;
 
-    return texCoords - viewDir.xy * (height * heightScale);        
+    float numLayers = mix(maxLayers, minLayers, max(dot(vec3(0.0, 0.0, 1.0), viewDir), 0.0));
+
+    float layerDepth = 1.0 / numLayers;
+    float currentLayerDepth = 0.0;
+
+    vec2 P = viewDir.xy * heightScale;
+    vec2 deltaTexCoords = P / numLayers;
+
+    vec2 currentTexCoords = texCoords;
+    float currentDepthMapValue = texture(depthMap, currentTexCoords).r;
+
+    while (currentLayerDepth < currentDepthMapValue) {
+        currentTexCoords -= deltaTexCoords;
+        currentDepthMapValue = texture(depthMap, currentTexCoords).r;
+        currentLayerDepth += layerDepth;
+    }
+
+    vec2 prevTexCoords = currentTexCoords + deltaTexCoords;
+    float afterDepth  = currentDepthMapValue - currentLayerDepth;
+    float beforeDepth = texture(depthMap, prevTexCoords).r - currentLayerDepth + layerDepth;
+
+    float weight = afterDepth / (afterDepth - beforeDepth);
+    vec2 finalTexCoords = prevTexCoords * weight + currentTexCoords * (1.0 - weight);
+
+    return finalTexCoords;
 }
 
 void main() {
